442e0445fb1e53c19923ef93d22c11a98832fd22
Kubenic <jordan.venant@gmail.com>
2019-03-07 12:10:48 +0100

    Ajout de l'authentification JWT

--- api/src/Entity/User.php
+++ api/src/Entity/User.php
@@ -2,10 +2,13 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
+ * @ApiResource()
+ * @ORM\Table(name="user_account")
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  */
 class User implements UserInterface
