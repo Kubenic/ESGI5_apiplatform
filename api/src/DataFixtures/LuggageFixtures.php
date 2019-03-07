54fa63e6dc8bb940440dc90022cf5df219266cd1
AxelE <axel91evrard@gmail.com>
2019-03-07 17:07:32 +0100

    Resolve Fixture

--- api/src/DataFixtures/LuggageFixtures.php
+++ api/src/DataFixtures/LuggageFixtures.php
@@ -2,18 +2,29 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Ticket;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use App\Entity\Luggage;
 
-class LuggageFixtures extends Fixture
+class LuggageFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager)
     {
+        $tickets = $manager->getRepository(Ticket::class)->findAll();
+
         $luggage = new Luggage();
-        $luggage->setWeight('50');
+        $luggage->setWeight('50')->setTicket($tickets[0]);
         $manager->persist($luggage);
 
         $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return array(
+            TicketFixtures::class,
+        );
     }
 }
--- api/src/DataFixtures/TravelerFixtures.php
+++ api/src/DataFixtures/TravelerFixtures.php
@@ -5,20 +5,24 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use App\Entity\Traveler;
+use Faker\Factory;
 
 class TravelerFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        $traveler = new Traveler();
-        $traveler->setFirstname('Traveler1');
-        $traveler->setLastname('Flyer');
-        $traveler->setPhone('0608080808');
-        $traveler->setEmail('traveler1@gmail.com');
-        $traveler->setPassword('travelertest');
-        $traveler->setStatus('1');
-        $manager->persist($traveler);
-
+        $faker = Factory::create('fr_FR');
+        for ($i = 0; $i < 5; $i++) {
+            $traveler = new Traveler();
+            $traveler
+                ->setFirstname($faker->firstName)
+                ->setLastname($faker->lastName)
+                ->setPhone($faker->mobileNumber)
+                ->setEmail($faker->email)
+                ->setPassword('passwordaencodeplustard')
+                ->setStatus('1');
+            $manager->persist($traveler);
+        }
         $manager->flush();
     }
 }
