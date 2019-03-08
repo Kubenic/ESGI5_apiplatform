90fcac2c72aa129e7c2119255ed1d03d139c0b1c
AxelE <axel91evrard@gmail.com>
2019-03-08 08:45:52 +0100

    Custom Validator

--- api/src/Entity/Airport.php
+++ api/src/Entity/Airport.php
@@ -7,6 +7,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
+use App\Validator\Constraints\ContainsAlphanumeric;
 
 /**
  * @ApiResource()
@@ -36,12 +37,14 @@ class Airport
     /**
      * @ORM\Column(type="string", length=255)
      * @Assert\NotBlank()
+     * @ContainsAlphanumeric()
      */
     private $country;
 
     /**
      * @ORM\Column(type="string", length=255)
      * @Assert\NotBlank()
+     * @ContainsAlphanumeric()
      */
     private $city;
 
